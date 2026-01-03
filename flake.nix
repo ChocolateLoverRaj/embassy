b056f2d1e518cde742c29736cf42d7e839597c07
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-01-03 10:53:35 -0800

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              (rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+                targets = [
+                  "thumbv7m-none-eabi"
+                  "thumbv6m-none-eabi"
+                ];
+              })
+              rust-analyzer
+              openocd
+              gdb
+              probe-rs-tools
+              elf2uf2-rs
+              flip-link
+              nil
+              nixd
+            ];
+          };
+      }
+    );
+}
